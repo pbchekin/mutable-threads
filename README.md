f2a00d2b133bf00276fb34b55df64f8e9c5d6b63
Pavel Chekin <pbchekin@gmail.com>
2018-08-23 20:48:53 -0700

    Mute for duration

--- README.md
+++ README.md
@@ -3,15 +3,22 @@
 This example runs several parallel threads, each prints to stdout a local counter.
 Also it runs a local TCP server, that listens, by default, on 127.0.0.1:7343 and accepts several simple commands:
 
-* `version`  - print the application version
 * `shutdown` - shut down the application
-* `mute`     - mute threads (disable output )
+* `mute N`   - mute threads (disable output) for N seconds
 * `unmute`   - unmute threads (enable output)
 
 Example:
 
+In the terminal 1:
+
 ```
-$ echo "mute" | nc 127.0.0.1 7343
-muted
+$ cargo run
+```
+
+In the terminal 2:
+
+```
+$ echo mute | nc 127.0.0.1 7343
+ok
 
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 extern crate linebased;
 extern crate threadpool;
+extern crate regex;
 
 use linebased::Server;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::{thread, time};
 use threadpool::ThreadPool;
+use regex::Regex;
+use std::time::{Duration, Instant};
 
 fn main() {
     let threads = 2;
     let pool = ThreadPool::new(threads);
     let one_second = time::Duration::from_secs(1);
-    let muted = Arc::new(AtomicBool::new(false));
+
+    let muted : Arc<RwLock<Option<Instant>>> = Arc::new(RwLock::new(None));
     let shutdown = Arc::new(AtomicBool::new(false));
 
     for id in 0..threads {
@@ -20,30 +24,44 @@ fn main() {
         pool.execute(move || {
             let mut counter = 0;
             while !shutdown.load(Ordering::Relaxed) {
-                if !muted.load(Ordering::Relaxed) {
+                let muted = match *muted.read().unwrap_or_else(|e| e.into_inner()) {
+                    None => false,
+                    Some(ts) => Instant::now() < ts
+                };
+
+                if !muted {
                     println!("{}: {}", id, counter);
                 }
+
                 counter = counter + 1;
                 thread::sleep(one_second);
             }
         })
     }
 
-    let mut server = Server::new(Default::default(), move |query| match query {
-        "version" => String::from("0.1.0"),
-        "shutdown" => {
-            shutdown.store(true, Ordering::Relaxed);
-            String::from("requested shutdown")
+    let mute_re = Regex::new(r"mute (\d+)").unwrap();
+
+    let mut server = Server::new(Default::default(), move |query| {
+        match query {
+            "shutdown" => {
+                shutdown.store(true, Ordering::Relaxed);
+            }
+            "unmute" => {
+                *muted.write().unwrap_or_else(|e| e.into_inner()) = None
+            }
+            _ => {
+                if let Some(mute_cap) = mute_re.captures(query) {
+                    if let Ok(delay) = mute_cap[1].parse::<u64>() {
+                        *muted.write().unwrap_or_else(|e| e.into_inner()) = Some(Instant::now() + Duration::from_secs(delay));
+                    } else {
+                        return String::from("invalid delay")
+                    }
+                } else {
+                    return String::from("unknown command")
+                }
+            }
         }
-        "mute" => {
-            muted.store(true, Ordering::Relaxed);
-            String::from("muted")
-        }
-        "unmute" => {
-            muted.store(false, Ordering::Relaxed);
-            String::from("unmuted")
-        }
-        _ => String::from("unknown command"),
+        "ok".to_string()
     }).unwrap();
 
     let handle = server.handle();
